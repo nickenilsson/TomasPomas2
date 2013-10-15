d0371bf8569595c4ed68269310c5dab46f2def23
Niklas Nilsson <nilsson.sthlm@gmail.com>
2013-10-15 16:53:02 +0200

    hojhoj

--- AtflickVersion2/AtflickVersion2/DropDownMenu.h
+++ AtflickVersion2/AtflickVersion2/DropDownMenu.h
@@ -2,12 +2,33 @@
 //  DropDownMenu.h
 //  DropDownNiklas
 //
-//  Created by Tomas Nilsson on 10/3/13.
+//  Created by Tomas Nilsson on 10/4/13.
 //  Copyright (c) 2013 Niklas Nilsson. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface DropDownMenu : UIButton
+@class DropDownMenu;
+
+@protocol DropDownMenuDelegate <NSObject>
+
+// Selection contains the user selected option or nil if nothing was selected
+- (void)dropDownControlView:(DropDownMenu *)view didFinishWithSelection:(id)selection;
+
+@optional
+
+// You can use this to disable scrolling on a tableView
+- (void)dropDownControlViewWillBecomeActive:(DropDownMenu *)view;
+
+@end
+
+
+@interface DropDownMenu : UIView
+
+- (void)setTitle:(NSString *)title;
+
+- (void)setSelectionOptions:(NSArray *)selectionOptions withTitles:(NSArray *)selectionOptionTitles;
+
+@property (weak, nonatomic) id <DropDownMenuDelegate> delegate;
 
 @end
