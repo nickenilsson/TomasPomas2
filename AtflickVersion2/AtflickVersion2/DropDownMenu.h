cb5b67423c810715c75e3340a68f43c052ea944b
Niklas Nilsson <nilsson.sthlm@gmail.com>
2013-10-15 16:45:26 +0200

    hej

---
+++ AtflickVersion2/AtflickVersion2/DropDownMenu.h
@@ -0,0 +1,13 @@
+//
+//  DropDownMenu.h
+//  DropDownNiklas
+//
+//  Created by Tomas Nilsson on 10/3/13.
+//  Copyright (c) 2013 Niklas Nilsson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DropDownMenu : UIButton
+
+@end
---
+++ AtflickVersion2/AtflickVersion2/DropDownMenu.m
@@ -0,0 +1,31 @@
+//
+//  DropDownMenu.m
+//  DropDownNiklas
+//
+//  Created by Tomas Nilsson on 10/3/13.
+//  Copyright (c) 2013 Niklas Nilsson. All rights reserved.
+//
+
+#import "DropDownMenu.h"
+
+@implementation DropDownMenu
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
